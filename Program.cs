187c508053688a5ddadb4dd93146a00c27b1094b
phuongthao002 <101983599+phuongthao002@users.noreply.github.com>
2022-04-04 14:57:09 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baitapvenha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        
+            {
+                TuDien tuDien = new TuDien();
+                bool Run = true;
+
+                while (Run)
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine("========================Chao mung ban den voi chuong trinh Tu Dien Viet - Anh===============");
+                    Console.WriteLine("\n0.Thoat chuong trinh");
+                    Console.WriteLine("\n1.Them tu vao tu dien");
+                    Console.WriteLine("\n2.Duyet danh sach tu dien");
+                    Console.WriteLine("\n3.Tim tu co trong tu dien");
+                    Console.Write("\nBan hay chon chuc nang ma ban muon: ");
+                    int Choice = Convert.ToInt32(Console.ReadLine());
+
+                    switch (Choice)
+                    {
+                        case 0:
+                            {
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.WriteLine("==========Doumo Arigatou Gozaimasu! See u later!============");
+                                Console.WriteLine("==========Chuong trinh tu dien Viet - Anh ket thuc==========");
+                                Run = false;
+                                break;
+                            }
+                        case 1:
+                            {
+                                Console.WriteLine("==========Them tu vao tu dien Viet - Anh============");
+                                tuDien.ThemTu();
+                                break;
+                            }
+                        case 2:
+                            {
+                                Console.WriteLine("==========Duyet danh sach tu dien Viet - Anh==========");
+                                tuDien.DuyetDanhSach();
+                                break;
+                            }
+                        case 3:
+                            {
+                                Console.WriteLine("=========Tim tu vung trong tu dien Viet - Anh==========");
+                                tuDien.TimTu();
+                                break;
+                            }
+                        default:
+                            {
+                                Console.WriteLine("Vui long nhap lai!");
+                                break;
+                            }
+                    }
+                }
+
+                Console.ReadLine();
+            }
+
+        }
+    
+}
---
+++ TuDien.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baitapvenha
+{
+    internal class TuDien
+    {
+
+
+        public Dictionary<string, string> DIC = new Dictionary<string, string>();
+
+        public void ThemTu(string a, string b)
+        {
+            this.DIC.Add(a, b);
+        }
+        public void ThemTu()
+        {
+            int n;
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write("Nhập số lượng từ muốn thêm đi người bạn:");
+            try
+            {
+                n = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                n = 1;
+            }
+            finally
+            {
+                Console.WriteLine("Ok bạn hiền, giờ thì nhập từ vào nhé!");
+            }
+            for (int i = 0; i < n; i++)
+            {
+                string a, b;
+                Console.Write("Bạn muốn thêm từ tiếng Việt: ");
+                a = Console.ReadLine();
+                Console.WriteLine("Từ này trong tiếng Anh có nghĩa là: ");
+                b = Console.ReadLine();
+                this.DIC.Add(a, b);
+            }
+        }
+
+        public void DuyetDanhSach()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            foreach (KeyValuePair<string, string> item in DIC)
+            {
+                Console.WriteLine(item.Key + "\t||\t" + item.Value);
+            }
+        }
+        public void TimTu()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            string a;
+            Console.WriteLine("Nhập từ bạn muốn dịch sang tiếng Anh:");
+            a = Console.ReadLine();
+            try
+            {
+                Console.Write("Trong tiếng Anh có nghĩa là: ");
+                Console.WriteLine(DIC[a]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Chắc là có gì đó không ổn rồi! Hệ thống k thể tìm thấy từ!");
+            }
+            finally
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+        }
+    }
+}
